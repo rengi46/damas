6b45f25e0b21c8e4e80218039771bdd79682aae7
Roger <rogerpuigdemasa@gmail.com>
2023-12-13 09:09:29 +0100

    change size

--- src/pages/Topos/Topos.jsx
+++ src/pages/Topos/Topos.jsx
@@ -6,15 +6,19 @@ export const Topos = () => {
   const style = {
     width: '100%',
     height: '100%',
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    border: 'none'
+    // position: 'absolute',
+    // top: '0',
+    // left: '0',
+    border: 'none',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
   return (
     <div style={style}>
       <Header/>
-      <iframe src="https://mellow-platypus-c20d1e.netlify.app" width="100%" height="100%"></iframe>
+      <iframe src="https://mellow-platypus-c20d1e.netlify.app" width="100%" height="50%"></iframe>
     </div>
   )
 }
