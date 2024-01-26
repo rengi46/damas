0cec3c34ab8c18e677b64410749bc450fb2cab89
Roger <rogerpuigdemasa@gmail.com>
2024-01-26 09:25:41 +0100

    build games banner

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import banner from '../../assets/img/slide-marca-ecoalf.jpg'
 const Header = () => {
   return (
     <div className='header'>
-      <img src={banner} alt="" width={"100%"} height={100}/>
+      <img src={banner} alt="" width={"100%"} height={"100%"}/>
       {/* <button className='btn_header' onClick={()=>{window.location = "/"}}>Go Back</button> */}
     </div>
   )
